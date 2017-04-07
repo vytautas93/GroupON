f183472d5dfb9584f0723454e03fa14f2e5f0ea2
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-07 12:01:56 +0000

    try with get country id

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -15,6 +15,8 @@ use GroupON\Contracts\GroupOnRepositoryContract;
 use Plenty\Modules\Order\Contracts\OrderRepositoryContract;
 use Plenty\Modules\Account\Address\Contracts\AddressRepositoryContract;
 use Plenty\Modules\Order\Models\Order;
+use Plenty\Modules\Order\Shipping\Countries\Contracts\CountryRepositoryContract;
+
 
 
 class ContentController extends Controller
@@ -28,20 +30,92 @@ class ContentController extends Controller
         OrderRepositoryContract $orderRepository,
         AddressRepositoryContract $addressRepository,
         ConfigRepository $configRepository,
+        CountryRepositoryContract $countryRepositoryContract,
         AuthHelper $authHelper
     )
     {
         $this->orderRepository = $orderRepository;
         $this->addressRepository = $addressRepository;
         $this->configRepository = $configRepository;
+        $this->countryRepositoryContract = $countryRepositoryContract;
         $this->authHelper = $authHelper;
     }
     
     public function test(Twig $twig):string
     {
         $groupOnOrders = $this->getGroupOnOrders();
+        foreach($groupOnOrders as $groupOnOrder)
+        {
+            $order = $this->authHelper->processUnguarded(
+                function () use ($order,$groupOnOrder) 
+                {
+                    $formatAddress = $this->checkAddress($groupOnOrder->customer->address1);
+                    if(is_array($formatAddress) && isset($formatAddress) )
+                    {
+                        $street = $formatAddress['Street'];
+                        $houseNumber = $formatAddress['HouseNumber'];
+                    }
+                    else
+                    {
+                        $street = $address;
+                        $houseNumber = $address;
+                    }           
+                    $countryISO = $groupOnOrder->customer->country;
+                    $country = $this->countryRepositoryContract->getCountryByIso($countryISO);
+                    
+                   /* $deliveryAddress = $this->addressRepository->createAddress([
+                        'name1' => $groupOnOrder->customer->name,
+                        'address1' => $street,
+                        'address2' => $houseNumber,
+                        'address3' => "Perrui",
+                        'address4' => "FreeFieldCreateSomething",
+                        'postalCode' => $groupOnOrder->customer->zip,
+                        'countryId' => 1,
+                        'stateId' => 1
+                    ]);
+                    
+                    $amounts = [];
+                    $amounts[] = [
+                        'currency' => 'EU',
+                        'priceOriginalGross' => 182.15,
+                        'priceOriginalNet' => 200.14
+                    ];
+                 
+                    $orderItems = [];
+                    $orderItems[] = [
+                        'typeId' => 11,
+                        'quantity' => 2,
+                        'orderItemName' => "HelloWorldItem",
+                        'itemVariationId' => 1033,
+                        'referrerId' => 1,
+                        'countryVatId' => 1,
+                        'amounts' => $amounts
+                    ];
+    
+                   
+                    $addOrder = $this->orderRepository->createOrder(
+                        [
+                            'typeId' => 1,
+                            'methodOfPaymentId' => 4040,
+                            'shippingProfileId' => 6,
+                            'statusId' => 5.0, 
+                            'ownerId' => 107,
+                            'plentyId' => 0,
+                            'orderItems' => $orderItems,
+                            'addressRelations' => [
+                                ['typeId' => 1, 'addressId' => $deliveryAddress->id],
+                                ['typeId' => 2, 'addressId' => $deliveryAddress->id],
+                            ]    
+                        ]);
+                    return $addOrder;*/
+                    return $country;
+                }
+            );
+        }
        
-        $templateData = array("supplierID" => json_encode($groupOnOrders[0]->orderid));
+       
+       
+        $templateData = array("supplierID" => json_encode($order));
         return $twig->render('GroupON::content.test',$templateData);
         
       /*  
@@ -141,6 +215,30 @@ class ContentController extends Controller
         
         return $groupOnData->data;
     }
+    
+    public function checkAddress($address)
+    {   
+        $removeNumber =  preg_match_all('/\d+/', $address, $matches);
+        
+        if ($removeNumber > 0 OR empty($address) == true)
+        {
+            $houseNumber = $matches[0][0];
+            $street = str_replace($houseNumber,'', $address);
+        }
+        else 
+        {
+            $houseNumber = $address;
+            $street = $address;
+        }
+        
+        $address = 
+        [
+            'HouseNumber' => $houseNumber,
+            'Street' => $street
+        ];
+        return $address;
+    }
+    
 
 
 
