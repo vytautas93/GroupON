6458ccb0da8bbbe4401aa4f4da6bb287dc40501d
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-11 07:23:30 +0000

    try add customer 3

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -62,7 +62,7 @@ class ContentController extends Controller
     public function test(Twig $twig):string
     {
         $data = array(
-            "externalId" => 123,
+            "referrerId" => 123,
             "typeId" => 1,
             "firstName"=>"Test1",
             "lastName"=>"LastNameTest",
