93f666d4f583a0a43bdd40dc0d90d0eb4e38e248
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-12 07:16:11 +0000

    try add cronč

--- src/Providers/GroupOnServiceProvider.php
+++ src/Providers/GroupOnServiceProvider.php
@@ -4,7 +4,6 @@ namespace GroupON\Providers;
  
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Cron\Services\CronContainer;
-
 use GroupON\Crons\SynchronizeGroupOnOrdersCron;
 use GroupON\Contracts\GroupOnRepositoryContract;
 use GroupON\Repositories\GroupOnRepository;
