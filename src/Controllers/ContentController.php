7f6327d756d2976b74b4bb2b60e61ca501d8277f
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-25 13:47:00 +0000

    try with filters 22

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -378,9 +378,9 @@ class ContentController extends Controller
             $contract = pluginApp(OrderRepositoryContract::class);
             $setFilter = $contract->setFilters(['externalOrderId' => (string)$orderID ]);
             $orderList = $contract->searchOrders();
-            $test = $orderList->page;
+            
             $this->getLogger(__FUNCTION__)->info('OrderLists',json_encode($orderList)); 
-            $this->getLogger(__FUNCTION__)->info('totalsCount',json_encode($test)); 
+            $this->getLogger(__FUNCTION__)->info('totalsCount',$orderList)); 
             if($orderList->totalsCount != 0)
             {
                 return true;
