d684736e3a23a42b18c6aa52ffee727d78eb4f58
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-26 12:00:56 +0000

    multiple stores 14

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -121,7 +121,7 @@ class ContentController extends Controller
 
     public function getGroupOnOrders($configuration)
     {
-        $url = 'https://scm.commerceinterface.com/api/v2/get_orders?supplier_id='.$configuration['supplierID'].'&token='.$configuration['token'].'&start_datetime=04/11/2017+00:01&end_datetime=04/11/2017+23:59';
+        $url = 'https://scm.commerceinterface.com/api/v2/get_orders?supplier_id='.$configuration['supplierID'].'&token='.$configuration['token'].'&start_datetime=03/01/2017+12:01&end_datetime=03/01/2017+15:00';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
