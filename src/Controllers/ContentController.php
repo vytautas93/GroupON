05b2dae5c5a46d26f6ee16a5d6f20373fd99004b
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-26 06:59:34 +0000

    try with filters 39

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -62,7 +62,7 @@ class ContentController extends Controller
         {
             
             $exists = $this->checkIfExists($groupOnOrder->orderid);
-           /* if ($exists == false) 
+            if ($exists == false) 
             {   
                 $this->getLogger(__FUNCTION__)->info("Nera", json_encode($exists)); 
                 $order = $this->generateOrder($groupOnOrder);
@@ -71,7 +71,7 @@ class ContentController extends Controller
             {
                 $this->getLogger(__FUNCTION__)->info("Yra", json_encode($exists)); 
                 $order = 'Nesukurti';
-            }*/
+            }
         }
         $templateData = array("supplierID" => json_encode($exists));
         return $twig->render('GroupON::content.test',$templateData);
@@ -378,6 +378,7 @@ class ContentController extends Controller
             $contract = pluginApp(OrderRepositoryContract::class);
             $setFilter = $contract->setFilters(['externalOrderId' => (string)$orderID ]);
             $orderList = $contract->searchOrders();
+            $orderList->totalsCount;
             $this->getLogger(__FUNCTION__)->error('OrderLists',json_encode($orderList)); 
             $this->getLogger(__FUNCTION__)->info('totalsCount',json_encode($orderList->isLastPage));
             
