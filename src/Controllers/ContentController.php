35f66575538106d6ff7567a8110605c973ef64e2
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-26 09:15:56 +0000

    json decode test7

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -379,6 +379,8 @@ class ContentController extends Controller
             $contract = pluginApp(OrderRepositoryContract::class);
             $setFilter = $contract->setFilters(['externalOrderId' => (string)$orderID ]);
             $orderList = $contract->searchOrders();
+            $test = $orderList['totalsCount'];
+            $this->getLogger(__FUNCTION__)->error('TEST',json_encode($test));
             $totalsCount = json_decode(json_encode($orderList),true);
             
             if($totalsCount['totalsCount'] > 0)
