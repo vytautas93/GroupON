d07f06a500c2f740e5a1103f51026511560d4f27
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-28 07:09:52 +0000

    work with database26

--- src/Migrations/CreateExpireTable.php
+++ src/Migrations/CreateExpireTable.php
@@ -4,11 +4,9 @@ namespace Groupon\Migrations;
  
 use Groupon\Models\Expire;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
- 
-use Plenty\Plugin\Log\Loggable;
+
 class CreateGrouponTable
 {
-    use Loggable;
     public function run(Migrate $migrate)
     {
         $migrate->createTable(Expire::class);
--- src/Migrations/CreateGrouponTable.php
+++ src/Migrations/CreateGrouponTable.php
@@ -5,10 +5,9 @@ namespace Groupon\Migrations;
 use Groupon\Models\StartTime;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
  
-use Plenty\Plugin\Log\Loggable;
+
 class CreateGrouponTable
 {
-   use Loggable;
     public function run(Migrate $migrate)
     {
         $migrate->deleteTable(StartTime::class);
