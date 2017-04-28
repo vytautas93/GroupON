5bfa687bf233d01fc40e071e08257027874686c4
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-28 06:31:17 +0000

    work with database21

--- src/Migrations/CreateGrouponTable.php
+++ src/Migrations/CreateGrouponTable.php
@@ -5,12 +5,14 @@ namespace Groupon\Migrations;
 use Groupon\Models\StartTime;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
  
-
+use Plenty\Plugin\Log\Loggable;
 class CreateGrouponTable
 {
-
+   use Loggable;
     public function run(Migrate $migrate)
     {
-        $migrate->createTable(StartTime::class);
+       /* $migrate->createTable(StartTime::class);*/
+        $test = $migrate->deleteTable(StartTime::class);
+        $this->getLogger(__FUNCTION__)->error("Migration",json_encode($test));   
     }
 }
--- src/Models/StartTime.php
+++ src/Models/StartTime.php
@@ -16,10 +16,10 @@ use Plenty\Modules\Plugin\DataBase\Contracts\Model;
 class StartTime extends Model
 {
     public $id = 0;
-    public $startTime = '';
-    public $createdAt = '';
+    public $plentyID = 0;
+    public $expiredtime = '';
+
  
-  
     public function getTableName(): string
     {
         return 'Groupon::StartTime';
