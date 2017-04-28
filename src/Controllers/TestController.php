97390eb9e782f18e6943f43d115ef1735dfc7b0f
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-04-28 14:18:19 +0000

    try to add payment 24

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -493,7 +493,7 @@ class TestController extends Controller
                             "mopId" => 4040,
                         ];
                     $createPayment = $paymentRepositoryContract->createPayment($data);
-                    $orderRelation = $paymentOrderRelationRepositoryContract->createOrderRelation($createPayment,$addOrder->id);
+                    $orderRelation = $paymentOrderRelationRepositoryContract->createOrderRelation($createPayment,$addOrder);
                     $this->getLogger(__FUNCTION__)->error("TEst",json_encode($orderRelation)); 
                     $exported = $this->markAsExported($groupOnOrder,$configuration);
                     return $addOrder;
