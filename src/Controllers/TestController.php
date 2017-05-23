504bf8923e22bd8fbbcd6b152630db5b5c766e8f
Vytautas <vytautas.sakalauskas@hashtages.com>
2017-05-23 14:16:37 +0000

    test10

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -591,8 +591,8 @@ class TestController extends Controller
         $response = curl_exec($ch); 
         curl_close($ch);      
         $groupOnData = json_decode($response);
-        $page = $groupOnData->meta->current_page;
-        $this->getLogger(__FUNCTION__)->error("Response",json_encode($page));
+        $page = $groupOnData->meta->no_of_pages;
+        $this->getLogger(__FUNCTION__)->error("Response",$page);
         return $page;
     }
 }
